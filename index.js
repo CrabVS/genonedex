53f62f8583275fcb42bc63744475efb5d2a2a049
Meece <andre.work.gm@gmail.com>
2024-01-12 11:05:24 +0200

    Add card generation and styling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const poke_container = document.querySelector('.container');
+const container = document.querySelector('.container');
 const pokemon_count = 151;
 const colors = {
     normal: '#A8A77A',
@@ -19,6 +19,9 @@ const colors = {
     dark: '#705746',
     steel: '#B7B7CE'
 }
+const opacity = '3a';
+
+const main_types = Object.keys(colors);
 
 const fetchPokemon = async function fetchAllPokemon() {
     for(let i = 1; i <= pokemon_count; i++) {
@@ -30,7 +33,40 @@ const getPokemon = async function getPokemonById(id) {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    createPokemonCard(data);
+}
+
+const createPokemonCard = function createPokemonCard(pokemon) {
+    const pokemonEl = document.createElement('div');
+    pokemonEl.classList.add('pokemon');
+    
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const id = pokemon.id.toString().padStart(3, '0');
+
+    const types = pokemon.types.map((type) => type.type.name);
+
+    const main_type = main_types.find(type => types.indexOf(type) > -1)
+    const color = colors[main_type];
+    pokemonEl.style.backgroundColor = color + opacity;
+
+    types.forEach((type, index) => {
+        types[index] = type[0].toUpperCase() + type.slice(1);
+    });
+    const type_display = types.join(' / ');
+
+    const pokemonInnerHTML = `
+    <div class="img-container">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="bulbasaur img">
+    </div>
+    <div class="details">
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">${type_display}</small>
+    </div>`
+
+    pokemonEl.innerHTML = pokemonInnerHTML;
+
+    container.appendChild(pokemonEl);
 }
 
 fetchPokemon();
